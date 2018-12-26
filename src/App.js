eb7f9aecb3277525d2fea804b29998162b24d7a6
AlexanderFengFeng <39276953+AlexanderFengFeng@users.noreply.github.com>
2018-12-26 03:45:41 -0800

    Added location input

--- src/App.js
+++ src/App.js
@@ -3,16 +3,35 @@ import './App.css'
 import { YELP_API_URL } from './profiles/dev'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: '',
+      placeholder: 'Enter City or Zip'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({location: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.getShopList();
+    event.preventDefault();
+  }
 
   async getShopList(){
-    let response = await fetch(YELP_API_URL + '/boba/San Jose')
+    let response = await fetch(YELP_API_URL + '/boba/' + this.state.location)
     let shops = await response.json()
 
     let RNG = Math.floor(Math.random() * shops.data.length)
 
     let container = document.getElementById('shopNameContainer')
     container.innerHTML = shops.data[RNG].name
-    container.href = shops.data[RNG].url
+    container.href = shops.data[RNG].url      
   }
 
   render() {
@@ -21,12 +40,19 @@ class App extends Component {
         <header className="App-header">
           <h1 class="main-header">Where we goin&rsquo;  for<br/>bubble tea?</h1>
           <p class="we-out">We out to <a target="_blank" id='shopNameContainer'></a>!</p>
-          <p onClick={() => this.getShopList()} className='clickable brown-btn'>
-            See where we goin&rsquo;</p>
+
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              <input value={this.state.location} onChange={this.handleChange} placeholder={this.state.placeholder}/>
+            </label>
+            <button className='clickable brown-btn'>See where we droppin&rsquo;</button>
+          </form>
+
         </header>
       </div>
     );
   }
-}
 
+}
 export default App;
+
